1249bf3755d208aa3c2fc54adbcbe0b82353d053
Samiddha Chakrabarti <samiddha99@gmail.com>
2023-07-04 11:52:53 +0530

    change

--- project/settings.py
+++ project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -84,6 +85,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "project.wsgi.application"
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
